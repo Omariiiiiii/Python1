cc88a284f86cdf4641d4666d38c982064c1248da
Omariiiiiii <omari.elizbarashvili@gmail.com>
2024-02-09 12:44:15 +0100

    .

---
+++ ..py
@@ -0,0 +1,4 @@
+text= "0123456789abcdef"
+print(text[5:9])
+print(text[0:12])
+print(text[::2])
---
+++ method.py
@@ -0,0 +1,18 @@
+text= "abcdefghijklmnop"
+print(dir(text))
+help(text.isupper)
+print(text.isupper())
+print("ABC".isupper())
+print(text.upper())
+print(text.upper().isupper())
+
+text.upper = text.upper()
+print(text, new_text)
+
+new_text = text.upper(
+print(text, new_text)
+print("bannannana".count("n"))
+print("bannannana".count("ann"))
+print("bannannana".replace("ana", "XXYYZZ"))
+sentence = "hello, kind-sir; How many! Ibe of service otday?"
+
